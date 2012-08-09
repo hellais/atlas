f518d2c2c28365542dd8ade0ad1a8c46401a8f92
Karsten Loesing <karsten.loesing@gmx.net>
2012-08-09 16:27:44 +0200

    Fix three columns in search results.
    Onionoo doesn't include or_address, or_port, and dir_port fields anymore,
    but or_addresses and dir_address.  Extract values from those fields for
    the search results table.

--- js/models/relay.js
+++ js/models/relay.js
@@ -138,6 +138,9 @@ define([
                 relay.bandwidth = bw;
                 relay.family = relay.family ? relay.family : null;
                 relay.bandwidth_hr = hrBandwidth(bw);
+                relay.or_address = relay.or_addresses ? relay.or_addresses[0].split(":")[0] : null;
+                relay.or_port = relay.or_addresses ? relay.or_addresses[0].split(":")[1] : 0;
+                relay.dir_port = relay.dir_address ? relay.dir_address.split(":")[1] : 0;
                 relay.country = relay.country.toLowerCase();
                 relay.countryname = CountryCodes[relay.country];
                 relay.uptime = model.parsedate(relay.last_restarted);
