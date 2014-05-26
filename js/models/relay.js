fe58cadbb5db75cb749f31a1803131ef29096630
Philipp Winter <phw@torproject.org>
2014-05-26 19:13:55 +0200

    Make Atlas understand "exit_policy_v6_summary".
    If present, the "exit_policy_v6_summary" data is displayed right underneath its
    IPv4 equivalent.  In addition, we reduce the vertical size of the <pre>
    environment so that the additional IPv6 box doesn't waste too much space.
    
    This should fix bug <https://bugs.torproject.org/12124>.

--- js/models/relay.js
+++ js/models/relay.js
@@ -141,6 +141,7 @@ define([
                     relay.dir_address = relay.dir_address ? relay.dir_address : null;
                     relay.exit_policy = relay.exit_policy ? relay.exit_policy : null;
                     relay.exit_policy_summary = relay.exit_policy_summary ?  relay.exit_policy_summary : null;
+                    relay.exit_policy_v6_summary = relay.exit_policy_v6_summary ?  relay.exit_policy_v6_summary : null;
                     relay.bandwidthr = relay.bandwidth_rate ? hrBandwidth(relay.bandwidth_rate) : null;
                     relay.bandwidthb = relay.bandwidth_burst ? hrBandwidth(relay.bandwidth_burst) : null;
                     relay.obandwidth = relay.observed_bandwidth ? hrBandwidth(relay.observed_bandwidth) : null;
